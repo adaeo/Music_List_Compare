41474ba856811ed0a01d4d9c849c00c7da5f7248
adaeo <wangj9507@gmail.com>
2022-06-16 21:31:52 +1000

    add try catch for test

--- test_output.py
+++ test_output.py
@@ -1,18 +1,20 @@
-import sys, json
+try:
+  import sys, json
+  # Function to calculate the sum of an array
+  def arraysum(arr):
+    return sum(arr)
 
-# Function to calculate the sum of an array
-def arraysum(arr):
-  return sum(arr)
+  # Get CLI arguments and parse to json
+  data = json.loads(sys.argv[1])
 
-# Get CLI arguments and parse to json
-data = json.loads(sys.argv[1])
+  # Get required field from data
+  array = data['array']
 
-# Get required field from data
-array = data['array']
+  # Calculate result
+  result = arraysum(array)
 
-# Calculate result
-result = arraysum(array)
-
-# Print data in stringified to parse in node server
-newdata = {'sum': result}
-print(json.dumps(newdata))
+  # Print data in stringified to parse in node server
+  newdata = {'sum': result}
+  print(json.dumps(newdata))
+except Exception as e:
+  print(e)
